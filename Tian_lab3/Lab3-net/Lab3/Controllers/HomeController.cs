2d8d9e9df8203bfdf3ee83a1ce18a8af1f4caa7d
Zhuang Tian <tian0044@algonquinlive.com>
2021-02-12 00:57:38 -0500

    First commit

---
+++ Tian_lab3/Lab3-net/Lab3/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Razor()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Count()
+        {
+            ViewData["bottles"] = Request.Form["bottles"];
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreatePerson()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DisplayPerson()
+        {
+            ViewData["firstname"] = Request.Form["firstname"];
+            ViewData["lastname"] = Request.Form["lastname"];
+            ViewData["age"] = Request.Form["age"];
+            ViewData["emailAddress"] = Request.Form["emailAddress"];
+            ViewData["dateofbirth"] = Request.Form["dateofbirth"];
+            ViewData["password"] = Request.Form["password"];
+            ViewData["description"] = Request.Form["description"];
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ Tian_lab3/Lab3-net/Lab3/Models/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab3.Models
+{
+    public class Person
+    {
+        private DataType dateOfBirth1;
+
+        public string firstName
+        {
+            get;
+            set;
+        }
+
+        public string lastName
+        {
+            get;
+            set;
+        }
+        public int age
+        {
+            get;
+            set;
+        }
+
+        public string emailAddress
+        {
+            get;
+            set;
+        }
+
+        public DataType dateOfBirth
+        {
+            get => dateOfBirth1;
+            set => dateOfBirth1 = value;
+        }
+
+        public string password
+        {
+            get;
+            set;
+        }
+
+        public string description
+        {
+            get;
+            set;
+        }
+    }
+}
